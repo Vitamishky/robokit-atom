51001dc632b2f3486d80f418bdd0d51c7bdf2d62
kondo <ryakin.is@phystech.edu>
2021-12-04 10:55:30 +0300

    UPD: Basketball

--- src/competitions/nodes/basketball_node.py
+++ src/competitions/nodes/basketball_node.py
@@ -6,12 +6,12 @@ import rospy
 import time
 import numpy as np
 from geometry_msgs.msg import Point
-from core.srv import ModelService, ServoService, WalkService
+from core.srv import ModelService, ServoService, WalkService, ImuService
 
 
 class Basketball:
     step_length = 36
-    const_step_length = 1
+    const_step_length = 12.5
     side_length = 10
     const_side_length = 1
     angle = 0.05
@@ -44,7 +44,7 @@ class Basketball:
             #rospy.loginfo(f"Got the ball: {self.ball[0]}, {self.ball[1]}")
             self.ball_self = self.get_ball_distance()
             self.distance_to_ball = sqrt(self.ball_self[0] ** 2 + self.ball_self[1] ** 2)
-            #rospy.loginfo(f"Got the ball in self coords: {self.ball_self[0]}, {self.ball_self[1]}")
+            rospy.loginfo(f"Got the ball in self coords: {self.ball_self[0]}, {self.ball_self[1]}")
 
 
     def update_basketcase(self, msg):
@@ -85,17 +85,23 @@ class Basketball:
         rospy.loginfo("Start finding ball")
         radians = Basketball.radians_search(5)
         l = []
-        while len(l) < 2:
-            for elem in radians:
-                self.move_head(elem[0], elem[1])
-                time.sleep(1)
-                if self.ball != (None, None):
-                    self.ball_self = self.get_ball_distance()
-                    l.append(self.ball_self)
+        for elem in radians:
+            self.move_head(elem[0], elem[1])
+            time.sleep(2)
+            if self.ball != (None, None):
+                l.append(self.ball_self)
+                print(self.ball_self)
             # choose the mediana of all balls in coordinates of robor and look at dispersy, check massive of all finded bolls
-                else:
-                    print("GOVNO")
+            else:
+                print("GOVNO")
+        for elem in radians:
+            self.move_head(elem[0], elem[1])
+            time.sleep(2)
+            if self.ball != (None, None):
+                l.append(self.ball_self)
+                print(self.ball_self)
         self.ball_coordinates = tuple(np.median(np.array(l), axis = 0))
+        print(len(l))
         print(self.ball_coordinates)
         if self.ball_coordinates != (None, None):
             self.flag_ball = True
@@ -139,12 +145,10 @@ class Basketball:
     def turn_to_ball(self):
         rospy.loginfo("Start turning to ball")
         # rotation = np.arctan(self.ball_self[1] / self.ball_self[0])
-        rotation = np.arctan(self.ball_coordinates[1] / self.ball_coordinates[0])
-        time_rotate = (math.fabs(rotation)) * Basketball.const_angle
-        print("XEEEEEERRRRR")
-        self.walk(True, 0, 0, Basketball.angle)
-        time.sleep(time_rotate)
-        self.walk(False, 0, 0, 0)
+        degree = np.arctan(self.ball_coordinates[1] / self.ball_coordinates[0]) * 180 / np.pi
+        rospy.loginfo(str(degree))
+        self.rotate(degree)
+        time.sleep(1)
 
     def turn_to_basketcase(self):
         rospy.loginfo("Start turning to basketcase")
@@ -169,11 +173,13 @@ class Basketball:
 
     def go_to_ball(self, percent_distance):
         rospy.loginfo("Start going to ball")
-        time_walk = 3 * percent_distance
+        time_walk = percent_distance * (self.distance_to_ball - 0.2) * self.const_step_length
+        print(self.distance_to_ball, time_walk)
         self.walk(True, Basketball.step_length, 0, 0)
         time.sleep(time_walk)
         # self.ball_coordinates
         self.walk(False, 0, 0, 0)
+        time.sleep(1)
 
     def go_to_basketcase(self, percent_distance):
         rospy.loginfo("Start going to basketcase")
@@ -205,59 +211,106 @@ class Basketball:
             time.sleep(time_walk)
             self.walk(False, 0, 0, 0)
 
+    def imu_client(self):
+        rospy.wait_for_service('imu_service')
+        try:
+            a = rospy.ServiceProxy('imu_service', ImuService)
+            return a()
+        except rospy.ServiceException as e:
+            print("Service call failed:", e)
+
+    def walk_client(self, n, step, side, ang):
+        rospy.wait_for_service('walk_service')
+        try:
+            a = rospy.ServiceProxy('walk_service', WalkService)
+            a(n, step, side, ang)
+        except rospy.ServiceException as e:
+            print("Service call failed:", e)
+
+    def rotate(self, to_rotate_deg):
+
+        imu_start = self.imu_client().x
+        imu_end = imu_start - to_rotate_deg
+        imu_end %= 360
+        rotation = 0.2
+        print(to_rotate_deg)
+
+        while True:
+            imu = self.imu_client()
+            print("imu: ", imu.x)
+            print("imu end: ", imu_end)
+            if np.abs(imu.x - imu_end) < 3:
+                break
+            
+            if imu.x - imu_end > 0:
+                print("Positive")
+                self.walk_client(True, 0, 0, rotation)
+            else:
+                print("Negative")
+                self.walk_client(True, 0, 0, -rotation)
+            
+        self.walk_client(False, 0, 0, 0)
+
 
 if __name__ == "__main__":
     rospy.init_node("basketball")
     basketball = Basketball()
     ball_sub = rospy.Subscriber('ball', Point, basketball.update_ball)
     basketcase_sub = rospy.Subscriber('basketcase', Point, basketball.update_basketcase)
-
+    while True:
+        if basketball.basketcase_self != (None, None):
+            print(basketball.ball_self)
+        else:
+            print("NO BALL")
     # Finding ball and putting it to self.ball_coordinates for future approach.
-    while not (basketball.flag_ball):
-        basketball.finding_ball()
-    print(basketball.flag_ball)
+    #while not (basketball.flag_ball):
+    #    basketball.finding_ball()
+    #print(basketball.flag_ball)
     
     #Approaching to the ball on 0.8 of distance
-    basketball.flag_ball = False
-    basketball.turn_to_ball()
-    basketball.go_to_ball(0.8)
+    #basketball.flag_ball = False
+    #basketball.turn_to_ball()
+    #basketball.go_to_ball(0.8)
     
     # Correct the ball position
-    while not (basketball.flag_ball): 
-        basketball.finding_ball()
-    print(basketball.flag_ball)
+    #while not (basketball.flag_ball): 
+    #    basketball.finding_ball()
+    #print(basketball.flag_ball)
     
     # Finally approach a ball
-    basketball.flag_ball = False
-    basketball.turn_to_ball()
-    basketball.go_to_ball(1)
+    #basketball.flag_ball = False
+    #basketball.turn_to_ball()
+    #basketball.go_to_ball(1)
     
     # Searching for basket
-    while not (basketball.flag_basketcase):
-        basketball.finding_basketcase()
-    print(basketball.flag_basketcase)
+    # while not (basketball.flag_basketcase):
+    #     basketball.finding_basketcase()
+    # print(basketball.flag_basketcase)
     
-    # Take ball and do several steps from ball holder
-    basketball.flag_basketcase = False
-    basketball.thinking_take()
+    # # Take ball and do several steps from ball holder
+    # basketball.flag_basketcase = False
+    # basketball.thinking_take()
     
-    # Searching for basket again
-    while not (basketball.flag_basketcase):
-        basketball.finding_basketcase()
-    print(basketball.flag_basketcase)
+    # # Searching for basket again
+    # while not (basketball.flag_basketcase):
+    #     basketball.finding_basketcase()
+    # print(basketball.flag_basketcase)
     
-    # Approach basket first time
-    basketball.flag_basketcase = False
-    basketball.turn_to_basketcase()
-    basketball.go_to_basketcase(0.8)
+    # # Approach basket first time
+    # basketball.flag_basketcase = False
+    # basketball.turn_to_basketcase()
+    # basketball.go_to_basketcase(0.8)
     
-    # Searching for basket again
-    while not (basketball.flag_basketcase):
-        basketball.finding_basketcase()
-    print(basketball.flag_basketcase)
+    # # Searching for basket again
+    # while not (basketball.flag_basketcase):
+    #     basketball.finding_basketcase()
+    # print(basketball.flag_basketcase)
     
-    # Finally approach basket
-    basketball.flag_basketcase  = False
-    basketball.go_to_basketcase(1)
-    # put the ball into basketcase
+    # # Finally approach basket
+    # basketball.flag_basketcase  = False
+    # basketball.go_to_basketcase(1)
+    # # put the ball into basketcase
     print("HE HIT THE BALL OR NO. I don't NO")
+    # while True:
+    #     n = int(input())
+    #     basketball.go_to_ball(1, n)
